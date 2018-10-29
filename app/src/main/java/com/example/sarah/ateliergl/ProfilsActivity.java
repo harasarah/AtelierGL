a5aca29a2e19be90ee58af8d3ba368c1cfcb5096
zainabrifi <zainabrifi52@gmail.com>
2018-10-29 18:41:25 +0000

    ajout page profils ( search bar + cellule + listVIew ) cellule = cellule.xml listview = profils_activity.xml

--- app/src/main/java/com/example/sarah/ateliergl/ProfilsActivity.java
+++ app/src/main/java/com/example/sarah/ateliergl/ProfilsActivity.java
@@ -3,7 +3,7 @@ package com.example.sarah.ateliergl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfilsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
