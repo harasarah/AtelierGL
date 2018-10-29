0462d642a58bc3fb22ec1642011f481057986530
Sarah <sarah.mouqine@gmail.com>
2018-10-29 19:28:49 +0000

    Profil Profil updatekk

---
+++ app/src/main/java/com/example/sarah/ateliergl/Activity_profile_update.java
@@ -0,0 +1,13 @@
+package com.example.sarah.ateliergl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_profile_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_update);
+    }
+}
