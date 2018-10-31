982bda44c68287864569b1b612e9629460bec6e8
othmane <othmane.feriani@gmail.com>
2018-10-31 14:49:54 +0100

    Splash Page 100% fonctionnelle + Deuxieme patch des couleurs

--- app/src/main/java/com/example/sarah/ateliergl/MainActivity.java
+++ app/src/main/java/com/example/sarah/ateliergl/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -17,12 +18,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
 
