e1a9dc5e414b718e8105725b1faea87f96f09915
Sarah <sarah.mouqine@gmail.com>
2018-12-03 22:50:42 +0000

    option call (page profil prestataire)

--- app/src/main/java/com/example/sarah/ateliergl/activity_profil.java
+++ app/src/main/java/com/example/sarah/ateliergl/activity_profil.java
@@ -1,13 +1,24 @@
 package com.example.sarah.ateliergl;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.effect.Effect;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
 
 public class activity_profil extends AppCompatActivity {
-
+    EditText phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +27,7 @@ public class activity_profil extends AppCompatActivity {
         Bundle extras= getIntent().getExtras();
         String nom = new String(((Bundle) extras).getString("nom"));
         String prenom = new String(((Bundle) extras).getString("prenom"));
-        Integer tel = new Integer(((Bundle) extras).getInt("tel"));
+        final Integer tel = new Integer(((Bundle) extras).getInt("tel"));
         String adresse = new String(((Bundle) extras).getString("adresse"));
 
 
@@ -29,7 +40,19 @@ public class activity_profil extends AppCompatActivity {
         t_prenom.setText(prenom);
         t_adresse.setText(adresse);
 
+        Button callButton = (Button)findViewById(R.id.btnCall);
+        callButton.setOnClickListener(new View.OnClickListener() {
 
+            public void onClick(View v) {
+                try {
+                    Uri phoneNumber = Uri.parse( "tel:"+tel );
+                    Intent call = new Intent(Intent.ACTION_CALL, phoneNumber);
+                    startActivity(call);
+                } catch (ActivityNotFoundException activityException) {
+                    Log.e("Calling a Phone Number", "Call failed", activityException);
+                }
 
+            }
+        });
     }
 }
