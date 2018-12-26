fdd2832982ffc8b8e9b87d54903f2e2f669248de
Sarah <sarah.mouqine@gmail.com>
2018-12-26 03:06:53 +0100

    Adding the prestataireList.java file

---
+++ app/src/main/java/com/example/sarah/ateliergl/PrestataireList.java
@@ -0,0 +1,18 @@
+package com.example.sarah.ateliergl;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class PrestataireList {
+    @SerializedName("List")
+    private ArrayList<Prestataire> prestataireList;
+
+    public ArrayList<Prestataire> getPrestataireArrayList() {
+        return prestataireList;
+    }
+
+    public void setPrestataireArrayList(ArrayList<Prestataire> employeeArrayList) {
+        this.prestataireList = employeeArrayList;
+    }
+}
