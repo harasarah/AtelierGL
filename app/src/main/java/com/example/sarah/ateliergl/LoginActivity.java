085396b0d2828433600319d6249510bfbd7216ee
zainabrifi <zainabrifi52@gmail.com>
2019-01-03 21:23:06 +0100

    authentification

--- app/src/main/java/com/example/sarah/ateliergl/LoginActivity.java
+++ app/src/main/java/com/example/sarah/ateliergl/LoginActivity.java
@@ -28,64 +28,69 @@ public class LoginActivity extends AppCompatActivity {
     TextView password_oublier;
     ArrayList<Prestataire> allProfils;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_login);
-    password =  findViewById(R.id.edittext_password);
-
-    login =  findViewById(R.id.edittext_email);
-    inscription = findViewById(R.id.inscription_link);
-
-
-    inscription.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-
-            Intent intent_inscription= new Intent(LoginActivity.this, InscriptionActivity.class);
-            startActivity(intent_inscription);
-        }
-    });
-
-
-        sign_in =  findViewById(R.id.confirmation_button);
-        sign_in.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public  void onClick (View view){
-            validate(login.getText().toString().trim(),password.getText().toString().trim());
-
-        }
-    });
-}
-
-    private void validate ( final String log , final String pass ){
-        final GetPrestataireDataService service = com.example.sarah.ateliergl.network.RetrofitInstance.getRetrofitInstance().create( GetPrestataireDataService.class );
+    private void validate(final String log, final String pass) {
+        final GetPrestataireDataService service = com.example.sarah.ateliergl.network.RetrofitInstance.getRetrofitInstance().create(GetPrestataireDataService.class);
         Call<PrestataireList> call = service.getPrestataireData();
-        call.enqueue( new Callback<PrestataireList>() {
+        call.enqueue(new Callback<PrestataireList>() {
             @Override
             public void onResponse(Call<PrestataireList> call, Response<PrestataireList> response) {
                 allProfils = (ArrayList<Prestataire>) response.body().getPrestataireArrayList();
+                boolean test = false;
                 for (Prestataire s : allProfils) {
-                    if ((log.equals(s.nom)) && (pass.equals(s.mdp))){
-                        Intent intent= new Intent(LoginActivity.this, ServiceActivity.class);
+                    if ((log.equals(s.mail)) && (pass.equals(s.mdp)) && (s.type_profil.equals("client"))) {
+                        Intent intent = new Intent(LoginActivity.this, ServiceActivity.class);
                         startActivity(intent);
+                        test = true;
+                        break;
                     }
                 }
-                Toast toast;
-                Context context = getApplicationContext();
-                String error_message = "Mot de passe incorrecte, réessayez";
-                toast = Toast.makeText(context,error_message,Toast.LENGTH_SHORT);
-                toast.show();
-
+                if (test == false ) {
+                    Toast toast;
+                    Context context = getApplicationContext();
+                    String error_message = "Mot de passe ou login incorrecte, réessayez";
+                    toast = Toast.makeText(context, error_message, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
+
             @Override
             public void onFailure(Call<PrestataireList> call, Throwable t) {
-                Toast.makeText( getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT ).show();
+                Toast.makeText(getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
 
-        } );
+        });
 
 
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        password = findViewById(R.id.edittext_password);
+
+        login = findViewById(R.id.edittext_email);
+        inscription = findViewById(R.id.inscription_link);
+
+
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent_inscription = new Intent(LoginActivity.this, InscriptionActivity.class);
+                startActivity(intent_inscription);
+            }
+        });
+
+
+        sign_in = findViewById(R.id.confirmation_button);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(login.getText().toString().trim(), password.getText().toString().trim());
+
+            }
+        });
+    }
+
 
 }
