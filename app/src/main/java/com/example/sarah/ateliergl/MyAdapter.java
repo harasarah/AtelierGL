b0dafa6dbca078e91f8b0a631968d84feeae711d
Othy <othmane.feriani@gmail.com>
2019-01-06 17:37:39 +0100

    Searchbar v1

--- app/src/main/java/com/example/sarah/ateliergl/MyAdapter.java
+++ app/src/main/java/com/example/sarah/ateliergl/MyAdapter.java
@@ -2,50 +2,24 @@ package com.example.sarah.ateliergl;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import com.example.sarah.ateliergl.Prestataire;
-import com.example.sarah.ateliergl.R;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 
-/* public class MyAdapter extends ArrayAdapter {
-    private ArrayList<Prestataire> prestataires;
-    public MyAdapter(Context context, int ressource, ArrayList<Prestataire> prestataires){
-        super(context, ressource, prestataires);
-        this.prestataires = prestataires;
-    }
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.PrestataireViewHolder> implements Filterable {
 
-    @NonNull
-    @Override
-    public View getView (int position, View convertView, ViewGroup parent){
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(getContext().LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate( R.layout.cellule_profile, parent, false);
-
-        de.hdodenhof.circleimageview.CircleImageView imageView = convertView.findViewById(R.id.ic_profil);
-        imageView.setImageResource(prestataires.get(position).getImageID());
-
-        TextView textView = (TextView) convertView.findViewById(R.id.name);
-        textView.setText((prestataires.get(position).getNom())+(prestataires.get(position).getPrenom()));
-
-        return convertView;
-    }
-} */
-
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.PrestataireViewHolder> {
-
-     ArrayList<Prestataire> dataList;
-     Context context;
+    ArrayList<Prestataire> dataList;
+    Context context;
 
     public MyAdapter(ArrayList<Prestataire> dataList, Context context) {
         this.dataList = dataList;
@@ -58,6 +32,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.PrestataireViewHol
         View view = layoutInflater.inflate(R.layout.cellule_profile, parent, false);
         return new PrestataireViewHolder(view);
     }
+
     @Override
     public void onBindViewHolder(PrestataireViewHolder holder, final int position) {
         holder.txtNom.setText(dataList.get(position).getNom());
@@ -68,23 +43,25 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.PrestataireViewHol
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent( context, activity_profil.class );
+                Intent intent = new Intent(context, activity_profil.class);
                 Prestataire pres = dataList.get(position);
-                intent.putExtra( "nom", pres.nom);
+                intent.putExtra("nom", pres.nom);
 
-                intent.putExtra( "prenom", pres.tel );
+                intent.putExtra("prenom", pres.tel);
                 intent.putExtra("cin", pres.cin);
-              //  intent.putExtra("imageID", pres.imageID);
+                //  intent.putExtra("imageID", pres.imageID);
                 intent.putExtra("adresse", pres.adresse);
                 intent.putExtra("tel", pres.tel);
                 intent.putExtra("password", pres.mdp);
                 intent.putExtra("profession", pres.type_profil);
-                intent.putExtra("rating",pres.rating);
-              //   intent.putExtra("description", pres.description);
+                intent.putExtra("rating", pres.rating);
+                //   intent.putExtra("description", pres.description);
                 context.startActivity(intent);
             }
         });
-    };
+    }
+
+    ;
 
 
     @Override
@@ -92,20 +69,116 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.PrestataireViewHol
         return dataList.size();
     }
 
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private class ViewHolder {
+        RelativeLayout nom_and_rating;
+        TextView nom;
+    }
+
+
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
+            holder = new ViewHolder();
+            convertView = layoutInflater.inflate(R.layout.cellule_profile, null);
+            holder.nom_and_rating = convertView.findViewById(R.id.nom_and_rating);
+            holder.nom = (TextView) convertView.findViewById(R.id.nom);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.nom.setText(dataList.get(position).nom);
+
+        holder.nom_and_rating.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Toast.makeText(context, dataList.get(position).nom, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                dataList = (ArrayList<Prestataire>) results.values; // has the filtered values
+                notifyDataSetChanged();  // notifies the data with new filtered values
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
+                ArrayList<Prestataire> FilteredArrList = new ArrayList<Prestataire>();
+
+                if (dataList == null) {
+                    dataList = new ArrayList<Prestataire>(dataList); // saves the original data in mOriginalValues
+                }
+
+                /********
+                 *
+                 *  If constraint(CharSequence that is received) is null returns the mOriginalValues(Original) values
+                 *  else does the Filtering and returns FilteredArrList(Filtered)
+                 *
+                 ********/
+                if (constraint == null || constraint.length() == 0) {
+
+                    // set the Original result to return
+                    results.count = dataList.size();
+                    results.values = dataList;
+                } else {
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < dataList.size(); i++) {
+                        String data = dataList.get(i).nom;
+                        if (data.toLowerCase().startsWith(constraint.toString())) {
+                            FilteredArrList.add(new Prestataire(dataList.get(i).nom, dataList.get(i).nom, dataList.get(i).nom, dataList.get(i).nom, dataList.get(i).tel, dataList.get(i).nom, dataList.get(i).nom, dataList.get(i).nom, dataList.get(i).nom, dataList.get(i).nom, dataList.get(i).rating));
+                        }
+                    }
+                    // set the Filtered result to return
+                    results.count = FilteredArrList.size();
+                    results.values = FilteredArrList;
+                }
+                return results;
+            }
+        };
+        return filter;
+    }
+
+
     class PrestataireViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txtNom, txtPrenom , txtPhone, txtCin;
+        TextView txtNom, txtPrenom, txtPhone, txtCin;
         RatingBar ratingBar;
 
         PrestataireViewHolder(View itemView) {
             super(itemView);
             txtNom = (TextView) itemView.findViewById(R.id.nom);
             ratingBar = itemView.findViewById(R.id.ratingx);
-          //  txtPrenom = (TextView) itemView.findViewById(R.id.prenom);
-         //  txtPhone = (TextView) itemView.findViewById(R.id.txt_employee_phone);
+            //  txtPrenom = (TextView) itemView.findViewById(R.id.prenom);
+            //  txtPhone = (TextView) itemView.findViewById(R.id.txt_employee_phone);
 
         }
     }
+
+
 }
 
 
--- app/src/main/java/com/example/sarah/ateliergl/ProfilsActivity.java
+++ app/src/main/java/com/example/sarah/ateliergl/ProfilsActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 
 import com.example.sarah.ateliergl.network.GetPrestataireDataService;
 import com.example.sarah.ateliergl.network.RetrofitInstance;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -34,29 +37,51 @@ public class ProfilsActivity extends AppCompatActivity {
     @BindView(R.id.recycler_view_prestataire_list)
     RecyclerView recyclerView;
     ArrayList<Prestataire> allProfils;
+    MaterialSearchBar searchBarmt;
+    RecyclerView listPrestataire;
 
 
     String se;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_profils );
-        //ButterKnife.bind (this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profils);
+
+        initialize();
+
+        searchBarmt.addTextChangeListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Call back the Adapter with current character to Filter
+                adapter.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+
 
         /*Create handle for the RetrofitInstance interface*/
-        GetPrestataireDataService service = RetrofitInstance.getRetrofitInstance().create( GetPrestataireDataService.class );
+        GetPrestataireDataService service = RetrofitInstance.getRetrofitInstance().create(GetPrestataireDataService.class);
 
         /*Call the method with parameter in the interface to get the employee data*/
         Call<PrestataireList> call = service.getPrestataireData();
 
         /*Log the URL called*/
-        Log.wtf( "URL Called", call.request().url() + "" );
+        Log.wtf("URL Called", call.request().url() + "");
 
-        call.enqueue( new Callback<PrestataireList>() {
+        call.enqueue(new Callback<PrestataireList>() {
             @Override
             public void onResponse(Call<PrestataireList> call, Response<PrestataireList> response) {
-                generatePrestataireList( response.body().getPrestataireArrayList() );
+                generatePrestataireList(response.body().getPrestataireArrayList());
 
                 allProfils = (ArrayList<Prestataire>) response.body().getPrestataireArrayList();
 
@@ -65,9 +90,9 @@ public class ProfilsActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<PrestataireList> call, Throwable t) {
-                Toast.makeText( ProfilsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT ).show();
+                Toast.makeText(ProfilsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
-        } );
+        });
         Button click_map = (Button) findViewById(R.id.map);
         click_map.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,13 +105,18 @@ public class ProfilsActivity extends AppCompatActivity {
         });
     }
 
+    private void initialize() {
+        searchBarmt = findViewById(R.id.searchBar);
+        listPrestataire = findViewById(R.id.recycler_view_prestataire_list);
+    }
+
     /*Method to generate List of employees using RecyclerView with custom adapter*/
     void generatePrestataireList(ArrayList<Prestataire> empDataList) {
         se = getIntent().getStringExtra("se");
         ArrayList<Prestataire> empDataListFiltered = new ArrayList<>();
         for (Prestataire p : empDataList) {
             if (p.service == null) continue;
-            if(p.service.equals(se)) empDataListFiltered.add(p);
+            if (p.service.equals(se)) empDataListFiltered.add(p);
         }
         /*empDataList.removeIf(new Predicate<Prestataire>() {
             @Override
@@ -98,33 +128,34 @@ public class ProfilsActivity extends AppCompatActivity {
                 return true;
             }
         });*/
-        recyclerView = (RecyclerView) findViewById( R.id.recycler_view_prestataire_list );
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_prestataire_list);
 
-        adapter = new MyAdapter( empDataListFiltered,this);
+        adapter = new MyAdapter(empDataListFiltered, this);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( ProfilsActivity.this );
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ProfilsActivity.this);
 
-        recyclerView.setLayoutManager( layoutManager );
+        recyclerView.setLayoutManager(layoutManager);
 
-        recyclerView.setAdapter( adapter );
+        recyclerView.setAdapter(adapter);
     }
+
     @Override
     protected void onRestart() {
         super.onRestart();
-        GetPrestataireDataService service = RetrofitInstance.getRetrofitInstance().create( GetPrestataireDataService.class );
+        GetPrestataireDataService service = RetrofitInstance.getRetrofitInstance().create(GetPrestataireDataService.class);
         Call<PrestataireList> call = service.getPrestataireData();
-        call.enqueue( new Callback<PrestataireList>() {
+        call.enqueue(new Callback<PrestataireList>() {
             @Override
             public void onResponse(Call<PrestataireList> call, Response<PrestataireList> response) {
-                generatePrestataireList( response.body().getPrestataireArrayList() );
+                generatePrestataireList(response.body().getPrestataireArrayList());
                 allProfils = (ArrayList<Prestataire>) response.body().getPrestataireArrayList();
             }
 
             @Override
             public void onFailure(Call<PrestataireList> call, Throwable t) {
-                Toast.makeText( ProfilsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT ).show();
+                Toast.makeText(ProfilsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
-        } );
+        });
     }
 
 
